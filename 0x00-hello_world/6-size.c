ee1d6d674a87f30c88b74ad809fe135f293a45bc
Kamdem-cmd <kamdemkevin87@gmail.com>
2025-02-03 04:11:13 +0100

    programmes C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
+	printf("size of a int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	return (0);
+}
 
