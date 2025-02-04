fbb5a777ba690481006657689fea6bb387b0cf24
Kamdem-cmd <kamdemkevin87@gmail.com>
2025-02-04 18:04:47 +0100

    ajout 8-print_base16.c 9-print_comb.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,22 @@
-fdsjks
+#include <stdlib.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char ch = '0';
+
+	while (ch != '9' + 1)
+	{
+		putchar(ch);
+		ch += 1;
+	}
+	ch = 'a';
+	while (ch != 'f' +1)
+	{
+		putchar(ch);
+		ch += 1;
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,22 @@
-qsd
+#include <stdlib.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char ch = '0';
+	
+	while (ch != '9' + 1)
+	{
+		putchar(ch);
+		if (ch == '9')
+		{
+			break;
+		}
+		ch += 1;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
