02acb33971699bfea5f5496bd8956e9a81b5972a
Kamdem-cmd <kamdemkevin87@gmail.com>
2025-05-02 08:03:33 +0100

    New commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,1 +1,40 @@
 #include "main.h"
+
+/**
+ * _strlen_rec - returns the length of a string (helper)
+ * @s: the string
+ * Return: length
+ */
+int _strlen_rec(char *s)
+{
+    if (*s == '\0')
+        return (0);
+    return (1 + _strlen_rec(s + 1));
+}
+
+/**
+ * _check_pal - recursively checks for palindrome
+ * @s: the string
+ * @debut: start index
+ * @fin: end index
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int _check_pal(char *s, int debut, int fin)
+{
+    if (debut >= fin)
+        return (1);
+    if (s[debut] != s[fin])
+        return (0);
+    return (_check_pal(s, debut + 1, fin - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: the string
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+    int len = _strlen_rec(s);
+    return (_check_pal(s, 0, len - 1));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,28 @@
+#include "main.h"
+
+/**
+ * check_prime - recursively checks if n is divisible by any number
+ * @n: number to check
+ * @i: current divisor
+ * Return: 0 if divisible, 1 if prime
+ */
+int check_prime(int n, int i)
+{
+    if (i * i > n)
+        return (1);
+    if (n % i == 0)
+        return (0);
+    return (check_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to check
+ * Return: 1 if prime, 0 otherwise
+ */
 int is_prime_number(int n)
 {
-    
+    if (n <= 1)
+        return (0);
+    return (check_prime(n, 2));
 }
